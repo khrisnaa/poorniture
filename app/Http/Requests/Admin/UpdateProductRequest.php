d39e74eafa8e971c9895aae0a7b094f3cc918e5a
khrisnaa <igdkhrisnaaa@gmail.com>
2025-04-06 21:39:10 +0800

    refactor: request

---
+++ app/Http/Requests/Admin/UpdateProductRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255', Rule::unique('products', 'name')->ignore($this->route('product')->id)],
+            'category_id' => ['required', 'exists:categories,id'],
+            'price' => ['required', 'numeric', 'min:0'],
+            'stock' => ['required', 'integer', 'min:0'],
+            'description' => ['required', 'string'],
+            'thumbnail' => ['nullable', 'file', 'mimes:jpeg,png,jpg,gif', 'max:10240'],
+            'images' => ['nullable', 'array'],
+            'images.*' => ['file', 'mimes:jpeg,png,jpg,gif', 'max:10240'],
+        ];
+    }
+}
